a62fbd4164f2dda0ac7d98e050d0f14084a248d8
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-01 14:05:40 +0530

    Added Python basics: input, operators, conditions

---
+++ basics/operators_and_conditions.py
@@ -0,0 +1,18 @@
+num=int(input("enter a number: "))
+
+if num%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
+num_1=int(input("enter your first number: "))
+num_2=int(input("enter your second number: "))
+
+
+if num_1>num_2:
+    print("the first number is greater")
+elif num_2>num_1:
+    print("the second number is greater")
+else:
+    print("both the number are equal")        
---
+++ basics/variables_and_input.py
@@ -0,0 +1,11 @@
+name = input("enter your name: ")
+age = int(input("enter your age: "))
+gpa = float(input("enter your gpa: "))
+is_alive=True
+
+
+print("your name is",name)
+print("your will be",age+1,"years old next year")
+print("your gpa is",gpa)
+print("am i alive?",is_alive)
+
