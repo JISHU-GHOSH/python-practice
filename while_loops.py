56a18626cf0e86dee38967c488a01284094ae44a
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-02 13:06:00 +0530

    removed while_loops.py from root directory

--- while_loops.py
+++
@@ -1,5 +0,0 @@
-num=int(input("enter a number between 1-10: "))
-while num<1 or num>10:
-    print("invalid input")
-    num=int(input("enter a number between 1-10: "))
-print("the number you enter is",num)    
