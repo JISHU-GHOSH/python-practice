56bac2fb7e4a9fb428331e5b8df24c0d231a6012
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-03 10:41:26 +0530

    added for loop practice program

---
+++ basics/for_loops.py
@@ -0,0 +1,20 @@
+for x in range(0,5):
+    print(x)
+print()    
+
+for x in range(10,0,-1):
+    print(x)
+print()    
+
+for x in range(1,20):
+    if x ==10:
+        continue
+    else:
+        print(x)
+print()        
+
+for x in range(1,20):
+    if x ==10:
+        break
+    else:
+        print(x) 
