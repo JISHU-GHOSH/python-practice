9f10863f406a29bb4e277b7067bf27c42b5c82e2
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-04 08:45:37 +0530

    added nested_loops.py

---
+++ basics/nested_loops.py
@@ -0,0 +1,8 @@
+row=int(input("enter the number of rows: "))
+columb=int(input("enter the number of couumb: "))
+symbol=input("enter the symbol you want to use: ")
+
+for x in range(row):
+    for j in range(columb):
+        print(symbol,end="")
+    print()    
