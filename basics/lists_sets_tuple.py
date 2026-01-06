82bd067e517ef1b75f7a71812ed38b2dced44d3f
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-06 21:15:28 +0530

    added lists tuple and sets practice file

---
+++ basics/lists_sets_tuple.py
@@ -0,0 +1,32 @@
+"""
+fruits=["apple","banana","mango","pineapple"]
+#for fruit in fruits:
+    #print(fruit)
+
+#print(len(fruits))    
+
+#print("watermleon" in fruits)# check if present or not
+#fruits[0]="grape"# changed first element
+#print(fruits[0])
+fruits.append("coconut")
+fruits.remove("apple")
+fruits.insert(2,"milkshake")
+fruits.sort()
+fruits.reverse()
+#fruits.clear()
+print(fruits.count("banana"))
+print(fruits)
+"""
+
+"""
+fruits={"apple","mango","banana","watermelon","cococnut"}
+fruits.add("tomato")
+fruits.remove("mango")
+fruits.pop()
+print(fruits)
+"""
+
+
+fruits=("apple","mango","banana","watermelon","cococnut")
+fruits.index("coconut")
+fruits.count("apple")
