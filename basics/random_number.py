d050feb5668e3fbff3048c48939a09073dd2ba30
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-09 16:15:48 +0530

    added random_number.py

---
+++ basics/random_number.py
@@ -0,0 +1,12 @@
+import random
+
+#random=random.randint(1,6)
+low=1
+high=100
+#random=random.randint(low,high)
+choice=["rock","paper","scissors"]
+cards=["2","3","5","6","0","3","4"]
+
+option=random.choice(cards)
+
+print(option)
