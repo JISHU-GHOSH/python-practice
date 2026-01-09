0d9fe295290ff4b5fff1b0f1e6ff9072f974804b
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-09 16:48:12 +0530

    added a number guessing game

---
+++ basics/number_guessing_game.py
@@ -0,0 +1,26 @@
+import random
+
+low=1
+high=50
+
+secret_number=random.randint(low, high)
+print("guess the number between 1 to 50")
+
+while True:
+    user_input=input("enter your number: ")
+    
+    if user_input.isdigit():
+        user_input=int(user_input)
+        if low<=user_input<=high:
+            if secret_number==user_input:
+                print("that was correct!")
+                break
+            else:
+                if user_input>secret_number:
+                    print("your guess is too high, try again")
+                else:
+                    print("your guess is too low, try again")
+        else:
+            print(f"please enter a number between {low} and {high}")
+    else:
+        print("invalid input! Please enter a valid number")
