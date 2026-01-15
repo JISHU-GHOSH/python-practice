5b7a1f23dbb53a6b8ce8416ccbc06bca72b9d02d
JISHU-GHOSH <jishughosh698@gmail.com>
2026-01-15 18:58:33 +0530

    added iterable.py

---
+++ basics/iterable.py
@@ -0,0 +1,22 @@
+food_items = ["pizza", "pasta", "burger", "salad", "tacos"]
+
+#for food in food_items:
+   # print(food,end=" ")
+    
+#for food in reversed(food_items):
+   # print(food)
+
+food_set = {"pizza", "pasta", "burger", "salad", "tacos"}
+
+#for food in food_set:
+    #print(food)
+#for food in reversed(food_set):#sets can be rversed print
+   # print(food)    
+food_dict = {"a": 1, "b": 2, "c": 3, "d": 4}
+
+#for key, value in food_dict.items():
+    #print(f"{key} : {value}")
+
+name="raj pal"
+for _ in name:
+    print(_,end=" ")
